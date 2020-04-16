014e793c8c34890dd6dd9cee9e397ee5e82233db
孙乾 <sunqian@hx-soft.cn>
2020-04-16 11:31:39 +0800

    修改为mybatis-plus

---
+++ src/main/java/com/hxoms/MybatisGeneratorMain.java
@@ -0,0 +1,34 @@
+package com.hxoms;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGeneratorMain {
+    public static void main(String[] args) {
+        MybatisGeneratorMain main = new MybatisGeneratorMain();
+        try {
+            main.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        InputStream inputStream = MybatisGeneratorMain.class.getClassLoader().getResourceAsStream("generatorConfig.xml");
+        Configuration config = cp.parseConfiguration(inputStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
