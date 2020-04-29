f2353d03e556190c9ccc7a52f7561604c74c3d63
孙乾 <sunqian@hx-soft.cn>
2020-04-29 11:22:09 +0800

    输出异常堆栈信息,帮助快速定位异常位置

---
+++ src.support/com/hxoms/support/b01/entity/B01Tree.java
@@ -0,0 +1,22 @@
+package com.hxoms.support.b01.entity;
+
+import com.hxoms.common.tree.Tree;
+
+/**
+ * 机构树
+ *
+ * @author sunqian
+ * @date 2020/4/24 15:26
+ */
+public class B01Tree extends Tree {
+
+    private String b0100 ;
+
+    public String getB0100() {
+        return b0100;
+    }
+
+    public void setB0100(String b0100) {
+        this.b0100 = b0100;
+    }
+}
--- src/main/java/com/hxoms/HxomsExceptionHandler.java
+++ src/main/java/com/hxoms/HxomsExceptionHandler.java
@@ -3,15 +3,20 @@ package com.hxoms;
 import com.hxoms.common.CustomMessageException;
 import com.hxoms.common.utils.Result;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class HxomsExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(HxomsExceptionHandler.class);
+
     @ExceptionHandler
     public Result handleException(Exception e) {
         String message = e.getMessage();
+        logger.error("异常信息：", e);
         if (e instanceof CustomMessageException) {
             if (StringUtils.isBlank(message)) {
                 message = "检查参数传递问题...";
