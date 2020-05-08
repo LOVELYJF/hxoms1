30ecfa2b10e0e98c9b4e9dd5353c78a5dfa47a27
LOVELYJF <2440539746@qq.com>
2020-05-08 09:44:41 +0800

    添加定时调度任务，配置

--- src/main/java/com/hxoms/HxomsApplication.java
+++ src/main/java/com/hxoms/HxomsApplication.java
@@ -3,8 +3,10 @@ package com.hxoms;
 import com.hxoms.common.utils.UserInfoUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HxomsApplication {
 
     public static void main(String[] args) {
