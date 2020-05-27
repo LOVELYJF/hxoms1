74afef35f82428007598177bf885212a3563fe20
rockstalgirl <lj.rockgirl@foxmail.com>
2020-05-27 08:47:44 +0800

    富文本文件生成

---
+++ src/main/java/com/hxoms/modules/file/entity/OmsReplaceKeywords.java
@@ -0,0 +1,115 @@
+package com.hxoms.modules.file.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hxoms.common.hxannotation.ColumnAnnotation;
+import com.hxoms.common.hxannotation.IdAnnotation;
+import com.hxoms.common.hxannotation.TableAnnotation;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@TableAnnotation(TableName = "oms_replace_keywords", TableDescription="出国境文件替换关键词")
+public class OmsReplaceKeywords {
+    @IdAnnotation
+    @ColumnAnnotation(FieldName = "ID",   FieldDescription="主键")
+    private String id;
+
+    @ColumnAnnotation(FieldName = "KEYWORD",   FieldDescription="关键词")
+    private String keyword;
+
+    @ColumnAnnotation(FieldName = "REPLACE_FIELD",   FieldDescription="替换字段名字")
+    private String replaceField;
+
+    @ColumnAnnotation(FieldName = "DESCRIPTION",   FieldDescription="描述")
+    private String description;
+
+    @ColumnAnnotation(FieldName = "TYPE",   FieldDescription="类型（因公、因私）")
+    private String type;
+
+    @ColumnAnnotation(FieldName = "CREATE_TIME",   FieldDescription="创建时间")
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private Date createTime;
+
+    @ColumnAnnotation(FieldName = "CREATE_USER",   FieldDescription="创建人")
+    private String createUser;
+
+    @ColumnAnnotation(FieldName = "MODIFY_TIME",   FieldDescription="修改时间")
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private Date modifyTime;
+
+    @ColumnAnnotation(FieldName = "MODIFY_USER",   FieldDescription="修改人")
+    private String modifyUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword == null ? null : keyword.trim();
+    }
+
+    public String getReplaceField() {
+        return replaceField;
+    }
+
+    public void setReplaceField(String replaceField) {
+        this.replaceField = replaceField == null ? null : replaceField.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getModifyUser() {
+        return modifyUser;
+    }
+
+    public void setModifyUser(String modifyUser) {
+        this.modifyUser = modifyUser == null ? null : modifyUser.trim();
+    }
+}
---
+++ src/main/java/com/hxoms/modules/file/mapper/OmsReplaceKeywordsMapper.java
@@ -0,0 +1,8 @@
+package com.hxoms.modules.file.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hxoms.modules.file.entity.OmsReplaceKeywords;
+
+public interface OmsReplaceKeywordsMapper extends BaseMapper<OmsReplaceKeywords> {
+
+}
