8204255cb86564b426fdfe3375815b96bad7363e
luoshuai <luoshuai@hx-soft.cn>
2020-05-30 17:16:21 +0800

    裸官模块信息修改

--- src/main/java/com/hxoms/modules/keySupervision/nakedOfficial/mapper/OmsSupNakedSignMapper.java
+++ src/main/java/com/hxoms/modules/keySupervision/nakedOfficial/mapper/OmsSupNakedSignMapper.java
@@ -3,7 +3,6 @@ package com.hxoms.modules.keySupervision.nakedOfficial.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hxoms.modules.keySupervision.nakedOfficial.entity.OmsSupNakedSign;
 
-import java.util.List;
 
 /**
  * <b>裸官信息业务层接口实现类</b>
@@ -11,10 +10,4 @@ import java.util.List;
  * @date 2020/5/11 16:57
  */
 public interface OmsSupNakedSignMapper extends BaseMapper<OmsSupNakedSign> {
-
-    /**
-     * <b>查询所有裸官的人员的a0100</b>
-     * @return
-     */
-    List<String> selectA0100List();
 }
