ed5c95edaa79979cc0ce16b7e1d69af8aa851655
LOVELYJF <2440539746@qq.com>
2020-06-02 19:35:08 +0800

    对，自动切换数据源对主数据源影响的修改，以及干部信息维护开发，对，配置文件进行分类

--- src/main/java/com/hxoms/modules/dataCapture/dataconfig/mapper/CutTargetDataSourceMapper.java
+++ src/main/java/com/hxoms/modules/dataCapture/dataconfig/mapper/CutTargetDataSourceMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Param;
  ***/
 public interface CutTargetDataSourceMapper {
 
+
     DataSource targetDataSource(@Param("tableName") String tableName, @Param("code") String code);
 
 }
