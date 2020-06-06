22e25a34cb03fe29534dd0c81ce1fa79278fda44
LOVELYJF <2440539746@qq.com>
2020-06-06 17:22:43 +0800

    添加 审批 状态

---
+++ src/main/resources/sql/20200606/审批状态.sql
@@ -0,0 +1,143 @@
+-- <! -- 因私 -->
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b', '因公出国', NULL, NULL, '因公出国', '因公出国', NULL, '', '1', '0', '10293', NULL, NULL, NULL, NULL, NULL, '', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b0', '未下发', NULL, NULL, '未下发', '未下发', NULL, '', '1', '0', '10294', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b1', '草稿', NULL, NULL, '草稿', '草稿', NULL, '', '1', '0', '10295', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b2', '带材料审核', NULL, NULL, '带材料审核', '带材料审核', NULL, '', '1', '0', '10296', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b3', '待征求意见', NULL, NULL, '待征求意见', '待征求意见', NULL, '', '1', '0', '10297', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b4', '待反馈意见', NULL, NULL, '待反馈意见', '待反馈意见', NULL, '', '1', '0', '10298', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b5', '待处长审批', NULL, NULL, '待处长审批', '待处长审批', NULL, '', '1', '0', '10299', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b6', '待部长审批', NULL, NULL, '待部长审批', '待部长审批', NULL, '', '1', '0', '10300', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'public_business', 'pu_b7', '已完结', NULL, NULL, '已完结', '已完结', NULL, '', '1', '0', '10301', NULL, NULL, NULL, NULL, NULL, 'pu_b', '0');
+
+
+ -- <! -- 因私 -->
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b', '因私出国', NULL, NULL, '因私出国', '因私出国', NULL, '', '1', '0', '10302', NULL, NULL, NULL, NULL, NULL, '', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b1', '草稿', NULL, NULL, '草稿', '草稿', NULL, '', '1', '0', '10303', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b2', '生成材料', NULL, NULL, '生成材料', '生成材料', NULL, '', '1', '0', '10304', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b3', '打印材料', NULL, NULL, '打印材料', '打印材料', NULL, '', '1', '0', '10305', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b4', '自评上报', NULL, NULL, '自评上报', '自评上报', NULL, '', '1', '0', '10306', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b5', '业务办理', NULL, NULL, '业务办理', '业务办理', NULL, '', '1', '0', '10306', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b6', '征求有关单位意见', NULL, NULL, '征求有关单位意见', '征求有关单位意见', NULL, '', '1', '0', '10307', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b7', '待反馈意见', NULL, NULL, '待反馈意见', '待反馈意见', NULL, '', '1', '0', '10307', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b8', '组织部审批', NULL, NULL, '组织部审批', '组织部审批', NULL, '', '1', '0', '10308', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b9', '核实批件', NULL, NULL, '核实批件', '核实批件', NULL, '', '1', '0', '10309', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b10', '制作备案表', NULL, NULL, '制作备案表', '制作备案表', NULL, '', '1', '0', '10310', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b11', '已办结', NULL, NULL, '已办结', '已办结', NULL, '', '1', '0', '10311', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b12', '待领证', NULL, NULL, '待领证', '待领证', NULL, '', '1', '0', '10312', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b13', '已领证', NULL, NULL, '已领证', '已领证', NULL, '', '1', '0', '10313', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'private_business', 'pri_b14', '撤销', NULL, NULL, '撤销', '撤销', NULL, '', '1', '0', '10313', NULL, NULL, NULL, NULL, NULL, 'pri_b', '0');
+
+
+
+-- <! -- 延期>
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b', '延期出国', NULL, NULL, '延期出国', '延期出国', NULL, '', '1', '0', '10314', NULL, NULL, NULL, NULL, NULL, '', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b1', '草稿', NULL, NULL, '草稿', '草稿', NULL, '', '1', '0', '10315', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b2', '生成材料', NULL, NULL, '生成材料', '生成材料', NULL, '', '1', '0', '10315', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b3', '打印材料', NULL, NULL, '打印材料', '打印材料', NULL, '', '1', '0', '10316', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b4', '自评上报', NULL, NULL, '自评上报', '自评上报', NULL, '', '1', '0', '10317', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b5', '业务办理', NULL, NULL, '业务办理', '业务办理', NULL, '', '1', '0', '10318', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b6', '征求有关单位意见', NULL, NULL, '征求有关单位意见', '征求有关单位意见', NULL, '', '1', '0', '10319', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b7', '待反馈意见', NULL, NULL, '待反馈意见', '待反馈意见', NULL, '', '1', '0', '10319', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b8', '组织部审批', NULL, NULL, '组织部审批', '组织部审批', NULL, '', '1', '0', '10320', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b9', '核实批件', NULL, NULL, '核实批件', '核实批件', NULL, '', '1', '0', '10321', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b10', '制作备案表', NULL, NULL, '制作备案表', '制作备案表', NULL, '', '1', '0', '10322', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b11', '已办结', NULL, NULL, '已办结', '已办结', NULL, '', '1', '0', '10323', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b12', '待领证', NULL, NULL, '待领证', '待领证', NULL, '', '1', '0', '10324', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b13', '已领证', NULL, NULL, '已领证', '已领证', NULL, '', '1', '0', '10325', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
+
+
+INSERT INTO `605hxoim`.`sys_dict_item` (`item_id`, `dict_code`, `item_code`, `item_name`, `item_num`, `item_levelcode`, `short_name`, `item_desc`, `comp_spell`, `logo_spell`, `is_use`, `is_leaf`, `orderindex`, `time_stamp`, `modify_user`, `modify_time`, `securitylevel`, `item_level`, `parent_item_code`, `is_deleted`)
+VALUES (REPLACE(UUID(),'-' ,''), 'delay_business', 'del_b14', '撤销', NULL, NULL, '撤销', '撤销', NULL, '', '1', '0', '10326', NULL, NULL, NULL, NULL, NULL, 'del_b', '0');
