d36749e5992c6d301a25bfd634c2e8d89506faf5
luoshuai <luoshuai@hx-soft.cn>
2020-06-28 19:45:32 +0800

    敏感国家地区限制内容

--- src/main/java/com/hxoms/modules/sensitiveCountry/sensitiveMaintain/mapper/OmsSensitiveMaintainMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hxoms.modules.sensitiveCountry.sensitiveMaintain.mapper;
-
-/**
- * <b>敏感国家维护数据持久层</b>
- * @author luoshuai
- * @date 2020/06/28
- */
-public interface OmsSensitiveMaintainMapper {
-}
