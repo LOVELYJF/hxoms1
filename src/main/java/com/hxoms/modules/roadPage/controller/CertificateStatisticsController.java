8b6fd62877d563792ad5de541a06041ac4dafbb6
wangxuechuan <Wangxc226688>
2020-07-09 12:00:47 +0800

    1、因公出国（境）预审批    省台办：接口开发    状态显：草稿,征求有关单位意见,待反馈意见,组织部初审,制作备案表,已办结,组织部审批,核实批件,生成材料,打印材料,自评,待领证,已领证,已撤消    数据结果集列表开接口开发

--- src/main/java/com/hxoms/modules/roadPage/controller/CertificateStatisticsController.java
+++ src/main/java/com/hxoms/modules/roadPage/controller/CertificateStatisticsController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/licenceStatistics")
 public class CertificateStatisticsController {
 
+
+
+
     
 
     @Autowired
