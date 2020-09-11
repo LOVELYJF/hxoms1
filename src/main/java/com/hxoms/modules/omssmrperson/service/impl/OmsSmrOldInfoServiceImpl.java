a79ab13465ce2e78e74d97fa32cc6579936edd73
gaozhenyuan <gaozhenyuan@hx-soft.cn>
2020-09-11 17:23:00 +0800

    因公团组接口细节完善

--- src/main/java/com/hxoms/modules/omssmrperson/service/impl/OmsSmrOldInfoServiceImpl.java
+++ src/main/java/com/hxoms/modules/omssmrperson/service/impl/OmsSmrOldInfoServiceImpl.java
@@ -1,24 +1,25 @@
 package com.hxoms.modules.omssmrperson.service.impl;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageInfo;
+import com.hxoms.common.utils.PageUtil;
 import com.hxoms.modules.omssmrperson.entity.OmsSmrOldInfo;
 import com.hxoms.modules.omssmrperson.mapper.OmsSmrOldInfoMapper;
 import com.hxoms.modules.omssmrperson.service.OmsSmrOldInfoService;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
+import java.util.List;
 
 @Service
 public class OmsSmrOldInfoServiceImpl extends ServiceImpl<OmsSmrOldInfoMapper, OmsSmrOldInfo> implements OmsSmrOldInfoService {
 
 
     @Override
-    public IPage<OmsSmrOldInfo> getSmrOldInfoById(String id){
-        return baseMapper.getSmrOldInfoList(id);
+    public PageInfo<OmsSmrOldInfo> getSmrOldInfoById(Integer pageNum, Integer pageSize,String id){
+        List<OmsSmrOldInfo> resultList = baseMapper.getSmrOldInfoList(id);
+        PageUtil.pageHelp(pageNum, pageSize);
+        PageInfo<OmsSmrOldInfo> pageInfo = new PageInfo(resultList);
+        return pageInfo;
     }
 
     @Override
