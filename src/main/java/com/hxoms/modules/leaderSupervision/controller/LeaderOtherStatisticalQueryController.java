071400e420104349e20433da132885857dd2668b
LOVELYJF <2440539746@qq.com>
2020-09-14 16:13:55 +0800

    修改 代办业务

--- src/main/java/com/hxoms/modules/leaderSupervision/controller/LeaderOtherStatisticalQueryController.java
+++ src/main/java/com/hxoms/modules/leaderSupervision/controller/LeaderOtherStatisticalQueryController.java
@@ -64,6 +64,7 @@ public class LeaderOtherStatisticalQueryController {
     @GetMapping("/selectBatchlist")
     public Result  selectBatchlist(){
 
+
         List<Map>  lists  =  leaderCommonMapper.selectLeaderBatch();
 
 
