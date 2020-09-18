c6b62ce21a06a5bb5f761177ec9559f6c46e3452
lijiaojiao@hx-soft.cn <lijiaojiao@hx-soft.cn>
2020-09-18 11:36:57 +0800

    修改代码

---
+++ src/main/java/com/hxoms/modules/omsregcadre/entity/OmsRegProcpersoninfoVO.java
@@ -0,0 +1,28 @@
+package com.hxoms.modules.omsregcadre.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hxoms.common.hxannotation.ColumnAnnotation;
+import com.hxoms.common.hxannotation.IdAnnotation;
+import com.hxoms.common.hxannotation.TableAnnotation;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 省管干部登记备案人员信息扩展类
+ * @author lijiaojiao
+ */
+public class OmsRegProcpersoninfoVO extends OmsRegProcpersoninfo{
+    //机构名称
+    private String b0101;
+
+    public String getB0101() {
+        return b0101;
+    }
+
+    public void setB0101(String b0101) {
+        this.b0101 = b0101;
+    }
+}
