1c3a4d72398e1245fcb79b27f03a3341f18a13c9
杨波 <yangbo@hx-soft.cn>
2020-09-22 17:05:27 +0800

    同步干部数据添加同步机构人员排序

--- src/main/java/com/hxoms/modules/dataCapture/masterdata/mapper/DataCaptureMapper.java
+++ src/main/java/com/hxoms/modules/dataCapture/masterdata/mapper/DataCaptureMapper.java
@@ -14,101 +14,139 @@ import java.util.Map;
 public interface DataCaptureMapper {
 
     List<Map> getMasterA01(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA01Count();
-    void      updateMasterA01(List<Map> updateList);
-    void      insertMasterA01(List<Map> insertList);
-    void      updateMasterA01ByIdCard(List<Map> idCardListByA01);
+
+    void updateMasterA01(List<Map> updateList);
+
+    void insertMasterA01(List<Map> insertList);
+
+    void updateMasterA01ByIdCard(List<Map> idCardListByA01);
 
     /**
-     *  A02表
-     * **/
+     * A02表
+     **/
     List<Map> getMasterA02(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA02Count();
+
     List<Map> getMasterA02();
-    void      updateMasterA02(List<Map> updateList);
-    void      insertMasterA02(List<Map> insertList);
+
+    void updateMasterA02(List<Map> updateList);
+
+    void insertMasterA02(List<Map> insertList);
 
     /**
-    *  A05表
-    * **/
+     * A05表
+     **/
     List<Map> getMasterA05(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA05Count();
-    void      updateMasterA05(List<Map> updateList);
-    void      insertMasterA05(List<Map> insertList);
+
+    void updateMasterA05(List<Map> updateList);
+
+    void insertMasterA05(List<Map> insertList);
 
 
     /**
-     *A06 表
-     *
+     * A06 表
      */
     List<Map> getMasterA06(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA06Count();
-    void      updateMasterA06(List<Map> updateList);
-    void      insertMasterA06(List<Map> insertList);
+
+    void updateMasterA06(List<Map> updateList);
+
+    void insertMasterA06(List<Map> insertList);
 
     /**
-     *A08 表
-     *
+     * A08 表
      */
     List<Map> getMasterA08(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA08Count();
-    void      updateMasterA08(List<Map> updateList);
-    void      insertMasterA08(List<Map> insertList);
+
+    void updateMasterA08(List<Map> updateList);
+
+    void insertMasterA08(List<Map> insertList);
 
 
     /**
-     *A14 表
-     *
+     * A14 表
      */
     List<Map> getMasterA14(@Param("offset") int offset, @Param("rows") int rows);
-    Integer getMasterA14Count();
-    void      updateMasterA14(List<Map> updateList);
-    void      insertMasterA14(List<Map> insertList);
 
+    Integer getMasterA14Count();
+
+    void updateMasterA14(List<Map> updateList);
+
+    void insertMasterA14(List<Map> insertList);
 
 
     /**
-     *A15 表
-     *
+     * A15 表
      */
     List<Map> getMasterA15(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA15Count();
-    void      updateMasterA15(List<Map> updateList);
-    void      insertMasterA15(List<Map> insertList);
+
+    void updateMasterA15(List<Map> updateList);
+
+    void insertMasterA15(List<Map> insertList);
 
     /**
      * A17表
-     * **/
+     **/
     List<Map> getMasterA17(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA17Count();
-    void      updateMasterA17(List<Map> updateList);
-    void      insertMasterA17(List<Map> insertList);
+
+    void updateMasterA17(List<Map> updateList);
+
+    void insertMasterA17(List<Map> insertList);
 
 
     /**
-     *A36 表
-     *
+     * A36 表
      */
     List<Map> getMasterA36(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA36Count();
-    void      updateMasterA36(List<Map> updateList);
-    void      insertMasterA36(List<Map> insertList);
+
+    void updateMasterA36(List<Map> updateList);
+
+    void insertMasterA36(List<Map> insertList);
 
     /**
-     *A30 表
-     *
+     * A30 表
      */
     List<Map> getMasterA30(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterA30Count();
-    void      updateMasterA30(List<Map> updateList);
-    void      insertMasterA30(List<Map> insertList);
+
+    void updateMasterA30(List<Map> updateList);
+
+    void insertMasterA30(List<Map> insertList);
 
     /**
-     *B01 表
-     *
+     * B01 表
      */
     List<Map> getMasterB01(@Param("offset") int offset, @Param("rows") int rows);
+
     Integer getMasterB01Count();
-    void      updateMasterB01(List<Map> updateList);
-    void      insertMasterB01(List<Map> insertList);
+
+    void updateMasterB01(List<Map> updateList);
+
+    void insertMasterB01(List<Map> insertList);
+
+    /**
+     * PersonOrgOrder 表
+     */
+    List<Map> getMasterPersonOrgOrder(@Param("offset") int offset, @Param("rows") int rows);
+
+    Integer getMasterPersonOrgOrderCount();
+
+    void updateMasterPersonOrgOrder(List<Map> updateList);
+
+    void insertMasterPersonOrgOrder(List<Map> insertList);
 }
