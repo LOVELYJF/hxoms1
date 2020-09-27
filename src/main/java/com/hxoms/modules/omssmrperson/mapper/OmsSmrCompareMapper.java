cc76314ed9128d55eccacaa3abb47ec042f1afb4
gaozhenyuan <gaozhenyuan@hx-soft.cn>
2020-09-27 16:55:24 +0800

    涉密人员管理功能完善

--- src/main/java/com/hxoms/modules/omssmrperson/mapper/OmsSmrCompareMapper.java
+++ src/main/java/com/hxoms/modules/omssmrperson/mapper/OmsSmrCompareMapper.java
@@ -1,8 +1,10 @@
 package com.hxoms.modules.omssmrperson.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.beust.jcommander.Parameter;
 import com.hxoms.modules.omssmrperson.entity.OmsSmrCompare;
 import com.hxoms.modules.omssmrperson.entity.OmsSmrCompareVO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +17,7 @@ import java.util.List;
 @Repository
 public interface OmsSmrCompareMapper extends BaseMapper<OmsSmrCompare> {
     //获取身份证纠正列表
-    List<OmsSmrCompareVO> getCompareIdCard(String b0100);
+    List<OmsSmrCompareVO> getCompareIdCard(@Param("b0100") String b0100);
     //判断导入的涉密人员信息能否匹配
     OmsSmrCompare getMatchingDate(String b0100, String idCardNumber);
 }
