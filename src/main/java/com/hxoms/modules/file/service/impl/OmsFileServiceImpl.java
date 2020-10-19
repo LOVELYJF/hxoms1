6749a0b51f2579862c20ae69cbbbbfa3c3f9797d
luoshuai <luoshuai@hx-soft.cn>
2020-10-19 20:43:07 +0800

    代码调整

--- src/main/java/com/hxoms/modules/file/service/impl/OmsFileServiceImpl.java
+++ src/main/java/com/hxoms/modules/file/service/impl/OmsFileServiceImpl.java
@@ -342,7 +342,7 @@ public class OmsFileServiceImpl implements OmsFileService {
         }
         //查询关键字
         QueryWrapper<OmsReplaceKeywords> queryWrapperKeyword = new QueryWrapper<>();
-        queryWrapperKeyword.eq("FILE_ID", omsFile.getFileId()==null?omsFile.getId():omsFile.getFileId());
+        queryWrapperKeyword.eq("FILE_ID", omsFile.getFileId()==null ? omsFile.getId() : omsFile.getFileId());
         List<OmsReplaceKeywords> omsReplaceKeywordList = omsReplaceKeywordsMapper.selectList(queryWrapperKeyword);
 
         result.put("omsFile", omsFile);
